c9b684deffb93e9d5c242b3f124cd74383e50a53
Muhammad Hamza <hamzahacker1626@gmail.com>
2021-01-31 22:43:53 +0500

    Create hop.py

---
+++ hop.py
@@ -0,0 +1,19 @@
+logo = """
+
+     db   db       .d88b.       d8888b. 
+     88   88      .8P  Y8.      88  `8D 
+     88ooo88      88    88      88oodD' 
+     88~~~88      88    88      88~~~   
+     88   88      `8b  d8'      88      
+     YP   YP       `Y88P'       88 
+
+-----------------------------------------------
+* Coded By  : Muhammad Hamza 
+* Facebook  : Muhammd Hamza
+* Github    : https://github.com/Hamzahash
+* Youtube   : H.O.P Programmers
+-----------------------------------------------"""
+print(banner)
+print ("")
+print(" Thanks for all of your but unfortunately this tool is depricated/removed by the author for some reasons that really hurts.")
+print("Bye Always Guys")
