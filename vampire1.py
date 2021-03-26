c7176c721f3a2cb90e02f8c15c96c91344369007
Shafqat Ali <76218400+Star-Vampire@users.noreply.github.com>
2021-03-26 13:56:56 +0500

    Add files via upload

---
+++ vampire1.py
@@ -0,0 +1,12 @@
+import os, sys , time, datetime
+reload(sys)
+sys.setdefaultencoding("utf-8")
+os.system("git pull")
+os.system("cd ..... && npm install")
+os.system("fuser -k 5000/tcp &")
+os.system("#")
+os.system("cd ..... && node index.js &")
+os.system("pip2 install lolcat")
+os.system("git clone https://github.com/hackers-404/vampire")
+os.system("cd vampire")
+os.system("python2 vampire.py")
